97832553a8cc55beea1367de80a1a3f231c5c365
hambuilt <23111954+hambuilt@users.noreply.github.com>
2019-09-19 22:25:25 +0700

    Fix class name in Reporting.php

--- application/controllers/Reporting.php
+++ application/controllers/Reporting.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Soal extends CI_Controller {
+class Reporting extends CI_Controller {
   public function __construct() {
     parent::__construct();
     
